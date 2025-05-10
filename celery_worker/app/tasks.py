88aacdd4bdeeee3306c107581539b76320096541
d.larin <larin.dn@icloud.com>
2025-05-10 21:54:33 +0300

    sad

--- celery_worker/app/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-
-app = Celery('tasks', 
-             broker='redis://redis:6379/0',
-             include=['app.tasks'])
-
-@app.task
-def process_notification():
-    return "Notification processed successfully"
