0807c5c9c1b8b4a6705c33a02b74f430fe20f6c1
d.larin <larin.dn@icloud.com>
2025-05-21 20:43:25 +0300

    asd

--- api_gateway/app/main.py
+++ api_gateway/app/main.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, HTTPS World!"
+@app.route("/gateway")
+def gateway():
+    return "API Gateway Works!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
