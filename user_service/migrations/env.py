0f0ebafa978ead83f4caa12dc5bcef1bac646179
d.larin <larin.dn@icloud.com>
2025-05-23 00:08:57 +0300

    cc

--- user_service/migrations/env.py
+++ user_service/migrations/env.py
@@ -1,19 +1,40 @@
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
 from alembic import context
-from models import Base
+import os
+import sys
 
+# Рассчитываем абсолютный путь к корню проекта
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.abspath(os.path.join(current_dir, "../../.."))
+sys.path.insert(0, root_dir)
+
+# Теперь можно импортировать модели
+from user_service.app.models import Base
+
+config = context.config
+fileConfig(config.config_file_name)
 target_metadata = Base.metadata
 
+
 def run_migrations_online():
-    from database import engine  # Импорт engine
-    with engine.connect() as connection:
+    """Run migrations in 'online' mode."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
         context.configure(
-            connection=connection,
-            target_metadata=target_metadata
+            connection=connection, target_metadata=target_metadata, compare_type=True
         )
+
         with context.begin_transaction():
             context.run_migrations()
 
+
 if context.is_offline_mode():
-    raise Exception("Offline migrations not supported.")
+    print("Offline migrations are not supported.")
 else:
     run_migrations_online()
