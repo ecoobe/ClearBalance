ade6536f2429556cefe7b208813c29ee8a0ab7e1
d.larin <larin.dn@icloud.com>
2025-05-23 22:03:25 +0300

    ads

--- api_gateway/app/main.py
+++ api_gateway/app/main.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from fastapi import APIRouter
+
 app = Flask(__name__)
+router = APIRouter()
+
 
 @app.route("/gateway")
 def gateway():
     return "API Gateway Works!"
 
+
+@router.get("/health")
+async def health_check():
+    return {"status": "ok"}
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=8000)
