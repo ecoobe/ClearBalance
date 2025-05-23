ca93d5060456f5366ef9710d8e24cea8341c3257
d.larin <larin.dn@icloud.com>
2025-05-23 22:10:07 +0300

    jh

--- api_gateway/app/main.py
+++ api_gateway/app/main.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+
+@app.route("/health")
+def health_check():
+    return jsonify({"status": "ok"}), 200
 
 
 @app.route("/gateway")
 def gateway():
     return "API Gateway Works!"
-
-
-@app.route("/health")
-def health_check():
-    return {"status": "ok"}
 
 
 if __name__ == "__main__":
