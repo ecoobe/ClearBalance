f08b4b8b41e8985d79e60382696e5e0895379b2d
d.larin <larin.dn@icloud.com>
2025-05-23 17:07:47 +0300

    asd

--- user_service/app/database.py
+++ user_service/app/database.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://myuser:mypassword@db:5432/mydb"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Declare Base here
+Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
--- user_service/app/models.py
+++ user_service/app/models.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from .database import Base  # Импорт Base из database.py
 
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, nullable=False)
     hashed_password = Column(String, nullable=True)
