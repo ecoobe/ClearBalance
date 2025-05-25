b14ed8f6a62032cabd13c35bc5a9fd01034ad5c6
d.larin <larin.dn@icloud.com>
2025-05-25 23:54:21 +0300

    as

---
+++ frontend/src/components/StubPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StubPage = ({ title }) => {
+  return (
+    <div className="stub-page">
+      <h2>{title}</h2>
+      <p>Раздел в разработке 🚧</p>
+    </div>
+  );
+};
+
+export default StubPage;
