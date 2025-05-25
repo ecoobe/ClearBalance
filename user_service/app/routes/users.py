41f8437413e4b3ba47c27a7db8fb15cb82dc637e
d.larin <larin.dn@icloud.com>
2025-05-25 14:40:52 +0300

    sd

--- user_service/app/routes/users.py
+++ user_service/app/routes/users.py
@@ -1,16 +1,18 @@
-# user_service/app/routes/users.py
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from jose import JWTError, jwt
 from ..database import get_db
 from ..models import User
 from ..schemas import UserResponse
+from app.routes.auth import oauth2_scheme, SECRET_KEY, ALGORITHM  # Добавлен импорт
 
 router = APIRouter()
 
 
 @router.get("/me", response_model=UserResponse)
 async def get_current_user(
-    db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)
+    db: Session = Depends(get_db),
+    token: str = Depends(oauth2_scheme),  # Теперь oauth2_scheme определен
 ):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
